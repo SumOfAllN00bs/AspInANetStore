7da4b57f71b4850e822ff6b9a8bd2b88d80f5e89
SumOfAllN00bs <discordshards@gmail.com>
2017-11-02 13:30:25 +1300

    Half way through making the post deployment script

--- AspInANetStoreDatabase/Products.sql
+++ AspInANetStoreDatabase/Products.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY,
 	[ProductName] VarChar(50),
 	[BaseCost] float default 0,
-	[Description] VarChar(150) NULL,
+	[Description] VarChar(MAX) NULL,
 	[Type] VarChar(50) NULL, 
     [Stocked] INT NULL default 0/*Was gonna use InStock but that felt like a boolean name, or quantity but that felt like it might mean the size of product like "a five pack"*/
 )
--- AspInANetStoreDatabase/Script.PostDeployment1.sql
+++ AspInANetStoreDatabase/Script.PostDeployment1.sql
@@ -1,14 +1,4 @@
-﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
+﻿/*Set up the Catalogue*/
 MERGE INTO Catalogue AS Target
 USING (VALUES 
 		(1, "Snakes","A dangerous pet to have, potentially venomous. Extra cost for defanging", 100)
@@ -22,3 +12,42 @@ WHEN NOT MATCHED BY TARGET THEN
 INSERT (Name, Description, ExtraCatalogueCost)
 VALUES (Name, Description, ExtraCatalogueCost);
 
+/*Set up the Customers*/
+MERGE INTO Customers AS Target
+USING (VALUES 
+		(1, "David", "Johnson", "Barry", "02112345478", "DbJohnson@example.com", "123 FourExample St, New York, New York, USA", 100.1, SysDateTime())
+		(2, "Lucy", "Garth", "Mary", "02112345448", "LMGarth@example.com", "13 Four St, Hampshire, New Jersey, USA", 0, SysDateTime())
+		(3, "Melissa", "Reece", "Betty", "02114345678", "MBReece@example.com", "23 Riddle Rd, Rothshire, New South Wales, Australia", 500, SysDateTime())
+		(4, "Harry", "Murray", "Bob", "02112344678", "HBMurray@example.com", "12 Needle St, MothManTown, Texas, USA", 0, SysDateTime())
+		(5, "Valerie", "Washington", "", "02144345678", "VWashington@example.com", "2 NumaNuma St, Victoria, Queensland, Australia", 2, SysDateTime())
+		(6, "Norman", "Chase", "J.", "02144344678", "NJ.Chase@example.com", "1 GroupOn St, Old York, Old York, Divided States Of Yugoslavia", 5000, SysDateTime())
+		(7, "Susie", "York", "T.", "02112222278", "ST.York@example.com", "3 Script St, Jafa York, Code, Britain", 0, SysDateTime())
+		(8, "Peter", "Galant", "F.", "02112322278", "PF.Galant@example.com", "124 Galant St, Peterton, State of Fitzpatrick, New Zealand", 3, SysDateTime())
+)
+AS SOURCE(Id, FirstName, LastName, MiddleName, Phone, Email, Address, BitCoinAmount, DateJoined)
+ON Target.Id = Source.Id
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (FirstName, LastName, MiddleName, Phone, Email, Address, BitCoinAmount, DateJoined)
+VALUES (FirstName, LastName, MiddleName, Phone, Email, Address, BitCoinAmount, DateJoined)
+
+/*Set up the Products*/
+MERGE INTO Products AS Target
+USING (VALUES 
+		(1, "Vipera aspis", 300, "Vipera aspis is a venomous viper species found in southwestern Europe", "Asp", 100)
+		(2, "King cobra (Ophiophagus hannah)", 500, "This serpent is the world's longest venomous snake. Adult king cobras are 3.18 to 4 m (10.4 to 13.1 ft) long.", "Cobra", 50)
+		(3, "Grass snake (Natrix natrix)", 450, "The grass snake is typically dark green or brown in colour with a characteristic yellow collar behind the head, which explains the alternative name ringed snake", "Grass Snake", 10)
+		(4, "Dwarf crocodile (Osteolaemus tetraspis)", 800, "The dwarf crocodile (Osteolaemus tetraspis), also known commonly as the African dwarf crocodile, broad-snouted crocodile, or bony crocodile, is an African crocodile that is also the smallest extant crocodile species.", "Crocodile", 20)
+		(5, "Slender-snouted crocodile (Mecistops cataphractus)",1000, "Slender-snouted crocodiles are native to freshwater habitats in central and western Africa. They are medium-sized crocodiles, typically slightly smaller than the Nile crocodile, but are larger than several other species of crocodilians.", "Crocodile",2)
+		(6, "Freshwater crocodile (Crocodylus johnsoni)", 900, "The freshwater crocodile is a relatively small crocodilian. Males can grow to 2.3–3 m (7.5–9.8 ft) long, while females reach a maximum size of 2.1 m (6.9 ft).", "Fresh water Crocodile", 80)
+		(7, "Leopard (Panthera pardus)", 400, "Compared to other members of Felidae, the leopard has relatively short legs and a long body with a large skull.", "Leopard", 10)
+		(8, "Cougar (Puma concolor)", 450, "The cougar is an ambush predator and pursues a wide variety of prey. Primary food sources are ungulates, particularly deer, but also livestock. It also hunts species as small as insects and rodents.", "Cougar", 1)
+		(9, "Cheetah (Acinonyx jubatus)", 300,"Cheetahs are active mainly during the day, with hunting their major activity. Adult males are sociable despite their territoriality, forming groups called 'coalitions'.", "Cheetah", 30)
+		(10, "Redback spider (Latrodectus hasseltii)", 100, "Mainly nocturnal, the female redback lives in an untidy web in a warm sheltered location, commonly near or inside human residences. It preys on insects, spiders and small vertebrates that become ensnared in its web.", "Spider", 14)
+		(11, "Goliath birdeater (Theraphosa blondi)", 120, "The goliath birdeater is native to the upland rain forest regions of northern South America: Suriname, Guyana, French Guiana, northern Brazil and southern Venezuela.", "Tarantula", 12)
+		(12, "Antilles pinktoe tarantula (Caribena versicolor)", 300, "Spiderlings of C. versicolor are bright blue, with a black tree trunk pattern on the abdomen. As they grow, they gradually lose their blue coloration; the carapace turns green, the abdomen red, and the legs turn green with pink tarsi and a covering of purple hairs.", "Tarantula", 7)
+)
+AS SOURCE(Id, ProductName, BaseCost, Description, Type, Stocked)
+ON Target.Id = Source.Id
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (ProductName, BaseCost, Description, Type, Stocked)
+VALUES (ProductName, BaseCost, Description, Type, Stocked)
