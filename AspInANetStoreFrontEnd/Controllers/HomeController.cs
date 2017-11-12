a92ef91e1fbfc943cd94a44251760cd83c545467
SumOfAllN00bs <discordshards@gmail.com>
2017-11-12 14:11:04 +1300

    Login stuff

--- AspInANetStoreFrontEnd/Controllers/HomeController.cs
+++ AspInANetStoreFrontEnd/Controllers/HomeController.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AspInANetStoreFrontEnd.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        static int saltLengthLimit = 32;
+        public ActionResult Index() => View();
+
+        public ActionResult Login() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string Username, string Password)
         {
+            if (ModelState.IsValid)
+            {
+                using(Models.AspInANetStoreDatabaseEntities1 db = new Models.AspInANetStoreDatabaseEntities1())
+                {
+                    var obj = db.Accounts.Where(a => a.Username == Username.Trim()).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        var PasswordHash = obj.PasswordHash;
+                        var Salt = obj.Salt;
+
+                        if(ComparePasswordHash(Password, Username, PasswordHash, Salt))
+                        {
+                            FormsAuthentication.SignOut();
+                            FormsAuthentication.SetAuthCookie(Username, false);
+                            Session["UserID"] = obj.Id;
+                            return RedirectToAction("Index");
+                        }
+                        else
+                        {
+                            ViewBag.Message = "Error #01: Access Denied! Wrong Credentials";
+                            return View();
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Error #02: Access Denied! Wrong Credentials";
+                        return View();
+                    }
+                }
+            }
             return View();
         }
 
@@ -25,6 +65,54 @@ namespace AspInANetStoreFrontEnd.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private static byte[] SaltMe(int maximumSaltLength)
+        {
+            var salt = new byte[maximumSaltLength];
+            using (var random = new RNGCryptoServiceProvider())
+            {
+                random.GetNonZeroBytes(salt);
+            }
+            return salt;
+        }
+
+        public static string SHA512_Hash(string password, string username, byte[] salt)
+        {
+            try
+            {
+                byte[] plainTextBytes = Encoding.UTF8.GetBytes(password + username);
+                byte[] plainTextWithSaltBytes = new byte[plainTextBytes.Length + salt.Length];
+
+                for (int i = 0; i < plainTextBytes.Length; i++) plainTextWithSaltBytes[i] = plainTextBytes[i];
+                for (int i = 0; i < salt.Length; i++) plainTextWithSaltBytes[plainTextBytes.Length + i] = salt[i];
+
+                HashAlgorithm hash = new SHA512Managed();
+                byte[] hashBytes = hash.ComputeHash(plainTextWithSaltBytes);
+                byte[] hashWithSaltBytes = new byte[hashBytes.Length + salt.Length];
+
+                for (int i = 0; i < hashBytes.Length; i++) hashWithSaltBytes[i] = hashBytes[i];
+                for (int i = 0; i < salt.Length; i++) hashWithSaltBytes[hashBytes.Length + i] = salt[i];
+
+                return Convert.ToBase64String(hashWithSaltBytes);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public static bool ComparePasswordHash(string password, string username, string OldHASHValue, byte[] SALT)
+        {
+            try
+            {
+                string expectedHashString = SHA512_Hash(password, username, SALT);
+                return (OldHASHValue == expectedHashString);
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
